9f85102cbb27a41ff5b0f7e27f43ec7118331fc0
Tyler jarrard <tjar2074@740s-pl-104.local>
2016-05-12 08:51:27 -0600

    game works

--- StupidGame/controller/StupidGame.cs
+++ StupidGame/controller/StupidGame.cs
@@ -18,6 +18,17 @@ namespace SampleGame.Controller
 
 		private Player player;
 
+		// Keyboard states used to determine key presses
+		private KeyboardState currentKeyboardState;
+		private KeyboardState previousKeyboardState;
+
+		// Gamepad states used to determine button presses
+		private GamePadState currentGamePadState;
+		private GamePadState previousGamePadState; 
+
+		// A movement speed for the player
+		private float playerMoveSpeed;
+
 
 		public StupidGame ()
 		{
@@ -36,6 +47,10 @@ namespace SampleGame.Controller
 			// TODO: Add your initialization logic here
             
 			player = new Player ();
+
+			// Set a constant player move speed
+			playerMoveSpeed = 8.0f;
+
 			base.Initialize ();
 		}
 
@@ -54,11 +69,46 @@ namespace SampleGame.Controller
 			//TODO: use this.Content to load your game content here 
 		}
 
+		private void UpdatePlayer(GameTime gameTime)
+		{
+
+			// Get Thumbstick Controls
+			player.Position.X += currentGamePadState.ThumbSticks.Left.X *playerMoveSpeed;
+			player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y *playerMoveSpeed;
+
+			// Use the Keyboard / Dpad
+			if (currentKeyboardState.IsKeyDown(Keys.Left) ||
+				currentGamePadState.DPad.Left == ButtonState.Pressed)
+			{
+				player.Position.X -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Right) ||
+				currentGamePadState.DPad.Right == ButtonState.Pressed)
+			{
+				player.Position.X += playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Up) ||
+				currentGamePadState.DPad.Up == ButtonState.Pressed)
+			{
+				player.Position.Y -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Down) ||
+				currentGamePadState.DPad.Down == ButtonState.Pressed)
+			{
+				player.Position.Y += playerMoveSpeed;
+			}
+
+			// Make sure that the player does not go out of bounds
+			player.Position.X = MathHelper.Clamp(player.Position.X, 0,GraphicsDevice.Viewport.Width - player.Width);
+			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0,GraphicsDevice.Viewport.Height - player.Height);
+		}
+
 		/// <summary>
 		/// Allows the game to run logic such as updating the world,
 		/// checking for collisions, gathering input, and playing audio.
 		/// </summary>
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+
 		protected override void Update (GameTime gameTime)
 		{
 			// For Mobile devices, this logic will close the Game when the Back button is pressed
@@ -69,7 +119,18 @@ namespace SampleGame.Controller
 			#endif
             
 			// TODO: Add your update logic here
-            
+			// Save the previous state of the keyboard and game pad so we can determinesingle key/button presses
+			previousGamePadState = currentGamePadState;
+			previousKeyboardState = currentKeyboardState;
+
+			// Read the current state of the keyboard and gamepad and store it
+			currentKeyboardState = Keyboard.GetState();
+			currentGamePadState = GamePad.GetState(PlayerIndex.One);
+
+
+			//Update the player
+			UpdatePlayer(gameTime);
+
 			base.Update (gameTime);
 		}
 
@@ -77,6 +138,7 @@ namespace SampleGame.Controller
 		/// This is called when the game should draw itself.
 		/// </summary>
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+
 		protected override void Draw (GameTime gameTime)
 		{
 			graphics.GraphicsDevice.Clear (Color.LavenderBlush);
