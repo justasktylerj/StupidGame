51b909e146096ffbe85acda877ac7108b6a3e661
Tyler jarrard <tjar2074@740s-pl-104.local>
2016-05-16 09:54:56 -0600

    final touches

--- StupidGame/controller/StupidGame.cs
+++ StupidGame/controller/StupidGame.cs
@@ -76,7 +76,7 @@ namespace SampleGame.Controller
 
 			Animation playerAnimation = new Animation();
 			Texture2D playerTexture = Content.Load<Texture2D>("Animation/Imported Paladin");
-			playerAnimation.Initialize(playerTexture, Vector2.Zero, 50, 60, 6, 80, Color.White, 1f, true);
+			playerAnimation.Initialize(playerTexture, Vector2.Zero, 50, 60, 6, 50, Color.White, 1f, true);
 
 			Vector2 playerPosition = new Vector2 (GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y
 				+ GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
